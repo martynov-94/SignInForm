785ce98464f9189cf24efd9c6bf2de9af8bb4584
martynov-94 <martynovsg94@gmail.com>
2022-02-02 17:18:15 +0300

    done

--- js/script.js
+++ js/script.js
@@ -5,13 +5,27 @@ const [
   logo,
   closeBtnLabel,
   passwordInput,
-  passwordContainer
+  passwordContainer,
+  resetForm,
+  signInForm,
+  emailInput,
+  emailInputContainer,
+  resetMsgContainer,
+  resetSubmitBtn,
+  resetMsgText
 ] = [
   ".social-actions",
   ".logo",
   ".close-btn-label",
   ".form__text-input_password",
-  ".form__pw-container"
+  ".form__pw-container",
+  ".reset-form",
+  ".sign-in-form",
+  ".reset-form__email-input",
+  ".reset-form__email-input-container",
+  ".reset-form__reset-msg-container",
+  ".reset-form__submit-btn",
+  ".reset-msg-text"
 ].map(selector => document.querySelector(selector));
 
 const mobileElements = [
@@ -38,6 +52,26 @@ passwordInput.oninput = () => {
   }
 }
 
+emailInput.oninput = () => {
+  if (!emailInput.value) {
+    emailInputContainer.classList.add("reset-form__email-input-container_empty");
+    emailInputContainer.classList.remove("reset-form__email-input-container_error");
+    resetSubmitBtn.classList.add("reset-form__submit-btn_disabled");
+    resetSubmitBtn.disabled = true;
+  } else if (emailInput.value === "test@gmail.com") {
+    emailInputContainer.classList.remove("reset-form__email-input-container_empty");
+    emailInputContainer.classList.remove("reset-form__email-input-container_error");
+    resetSubmitBtn.classList.remove("reset-form__submit-btn_disabled");
+    resetSubmitBtn.disabled = false;
+  } else {
+    emailInputContainer.classList.remove("reset-form__email-input-container_empty");
+    emailInputContainer.classList.add("reset-form__email-input-container_error");
+    resetSubmitBtn.classList.add("reset-form__submit-btn_disabled");
+    resetSubmitBtn.disabled = true;
+
+  }
+}
+
 function toggleElementsVisibility() {
   if (window.innerWidth >= breakpointLarge) {
     desktopElements.forEach(el => el.classList.remove("visually-hidden"));
@@ -50,4 +84,17 @@ function toggleElementsVisibility() {
 
 function togglePasswordVisibility() {
   passwordInput.type = (passwordInput.type === "password") ? "text" : "password";
+}
+
+function showResetForm() {
+  signInForm.classList.add("visually-hidden");
+  resetForm.classList.remove("visually-hidden");
+}
+
+function resetLoginAndPw() {
+  resetMsgContainer.classList.remove("visually-hidden");
+  emailInputContainer.classList.add("visually-hidden");
+  resetSubmitBtn.classList.add("visually-hidden");
+
+  resetMsgText.textContent += ` ${emailInput.value}`;
 }
