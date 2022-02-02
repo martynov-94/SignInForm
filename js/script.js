ba11772de42e1a3b9769858bad3a0e26d32d45fd
martynov-94 <martynovsg94@gmail.com>
2022-02-02 15:19:21 +0300

    sign-in done

--- js/script.js
+++ js/script.js
@@ -1,9 +1,17 @@
 const breakpointLarge = 768;
 
 const [
-  socialActions
+  socialActions,
+  logo,
+  closeBtnLabel,
+  passwordInput,
+  passwordContainer
 ] = [
-  ".social-actions"
+  ".social-actions",
+  ".logo",
+  ".close-btn-label",
+  ".form__text-input_password",
+  ".form__pw-container"
 ].map(selector => document.querySelector(selector));
 
 const mobileElements = [
@@ -11,12 +19,24 @@ const mobileElements = [
 ];
 
 const desktopElements = [
+  logo,
+  closeBtnLabel
 ];
 
 [
   "DOMContentLoaded",
   "resize", 
 ].forEach(event => window.addEventListener(event, toggleElementsVisibility));
+
+passwordInput.oninput = () => {
+  if (passwordInput.value === "error") {
+    passwordInput.classList.add("form__text-input_error");
+    passwordContainer.classList.add("form__pw-container_error");
+  } else {
+    passwordInput.classList.remove("form__text-input_error");
+    passwordContainer.classList.remove("form__pw-container_error");
+  }
+}
 
 function toggleElementsVisibility() {
   if (window.innerWidth >= breakpointLarge) {
@@ -26,4 +46,8 @@ function toggleElementsVisibility() {
     desktopElements.forEach(el => el.classList.add("visually-hidden"));
     mobileElements.forEach(el => el.classList.remove("visually-hidden"));
   }
+}
+
+function togglePasswordVisibility() {
+  passwordInput.type = (passwordInput.type === "password") ? "text" : "password";
 }
